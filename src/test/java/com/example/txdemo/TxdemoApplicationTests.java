2fa221ffd8f567c944d0dab42b1b73cd41f2e775
vernetto <Ernesto0z>
2020-09-20 18:22:23 +0200

    adding logs

--- src/test/java/com/example/txdemo/TxdemoApplicationTests.java
+++ src/test/java/com/example/txdemo/TxdemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.txdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @SpringBootTest
+@Slf4j
 class TxdemoApplicationTests {
     @Autowired
     BookRepository bookRepository;
@@ -17,34 +19,33 @@ class TxdemoApplicationTests {
     void contextLoads1() {
         System.out.println("contextLoads1");
         List<Book> allBooks = bookRepository.findAll();
-        System.out.println(allBooks.size());
+        log.info("size=" + allBooks.size());
         Book book = new Book();
         book.setTitle("ciao1");
         bookRepository.save(book);
         allBooks = bookRepository.findAll();
-        System.out.println(allBooks.size());
+        log.info("size=" + allBooks.size());
     }
 
     @Test
     @Transactional
     void contextLoads2() {
-        System.out.println("contextLoads2");
+        log.info("contextLoads2");
         List<Book> allBooks = bookRepository.findAll();
-        System.out.println(allBooks.size());
+        log.info("size=" + allBooks.size());
         Book book = new Book();
         book.setTitle("ciao2");
         bookRepository.save(book);
         allBooks = bookRepository.findAll();
-        System.out.println(allBooks.size());
-
+        log.info("size=" + allBooks.size());
     }
 
     @Test
     @Transactional
     void contextLoads3() {
-        System.out.println("contextLoads3");
+        log.info("contextLoads3");
         List<Book> allBooks = bookRepository.findAll();
-        System.out.println(allBooks.size());
+        log.info("size=" + allBooks.size());
     }
 
 }
